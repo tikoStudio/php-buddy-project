57463a22d581baf601ecbf95e4f556a994af7483
Caro03 <caroverstreken@hotmail.com>
2020-03-24 16:20:56 +0100

    Add settings.php in .gitignore file

--- settings/settings.php
+++ settings/settings.php
@@ -3,9 +3,9 @@
     const SETTINGS = [
         "db" => [
             "user"      => "root",
-            "password"  => "",
+            "password"  => "root",
             "host"      => "localhost",
             "port"      => 3306,
-            "dbname"    => "buddy-project"
+            "dbname"    => "buddy"
         ]
     ];
