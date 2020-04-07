22ee568b776b83137b90ac9e842fdb999d04fad5
tikoStudio <timothy.koenig@mail.com>
2020-04-07 17:18:49 +0200

    feature13: problem = buddyEmail

--- ajax/requestMatch.php
+++ ajax/requestMatch.php
@@ -1,7 +1,7 @@
 <?php
     include_once(__DIR__ . "../../classes/Buddy.php");
     include_once(__DIR__ . "../../functions.php");
-    if(!empty($_POST)) {
+    if(!empty($_POST['buddyEmail'])) {
 
         //new class object
         $requestMatch = new Buddy();
@@ -13,9 +13,17 @@
         $requestMatch->stopSearchingMatch($_POST['userId1']);
         $requestMatch->stopSearchingMatch($_POST['userId2']);
 
-        // mail user
-        //$requestMatch->sendMail($_POST['buddyEmail']);
-        //$test = $_POST['buddyEmail'];
+       /* // mail user
+        // get email adress
+        $email = $_POST['buddyEmail'];
+        // subject of mail
+        $subject = 'Buddy verzoek';
+        // message in mail
+        $message = "Een ijverige imd student heeft je een buddy request gestuurd!\r\nLog nu in om te kijken wie er met je wil connecteren.\r\nVeel plezier met potentiele buddy!";
+        // header with from which email
+        $headers = 'From: IMD BUDDIES APP';
+        mail($email, $subject, $message, $headers); 
+        echo "heyhoy sendmail functie is hier geraakt";*/
 
         //return success
         $response = [
