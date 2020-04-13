39e308291e27ae7a2955b75909baca8a58f31908
tikoStudio <timothy.koenig@mail.com>
2020-04-13 17:36:41 +0200

    feature16: added login limit

---
+++ classes/IpCheck.php
@@ -0,0 +1,97 @@
+<?php
+    include_once(__DIR__ . "./Db.php");
+
+    class IpCheck {
+        private $ip;
+        private $t;
+        private $diff;
+
+        public function getIp()
+        {
+                return $this->ip;
+        }
+
+        public function setIp($ip)
+        {
+                $this->ip = $ip;
+
+                return $this;
+        }
+
+        public function getT()
+        {
+                return $this->t;
+        }
+
+        public function setT($t)
+        {
+                $this->t = $t;
+
+                return $this;
+        }
+
+        public function getDiff()
+        {
+                return $this->diff;
+        }
+
+        public function setDiff($diff)
+        {
+                $this->diff = $diff;
+
+                return $this;
+        }
+
+        public function failedLogin() {
+            //db conn
+            $conn = Db::getConnection();
+            //insert query
+            $statement = $conn->prepare("insert into loginLimit (ipAddress, timeDiff) values (:ip, :t)");
+            $ip = $this->getIp();
+            $t = $this->getT();
+            $statement->bindParam(":ip", $ip);
+            $statement->bindParam(":t", $t);
+            $statement->execute();
+            $this->deleteFailedLogin();
+        }
+
+        public function deleteFailedLogin() {
+            //db conn
+            $conn = Db::getConnection();
+            //insert query
+            $statement = $conn->prepare("delete from loginLimit where ipAddress like :ip and timeDiff < :diff");
+            $ip = $this->getIp();
+            $diff = $this->getDiff();
+            $statement->bindParam(":ip", $ip);
+            $statement->bindParam(":diff", $diff);
+
+            $statement->execute();
+        }
+
+        public function deleteAllFailedLogin() {
+            //db conn
+            $conn = Db::getConnection();
+            //insert query
+            $statement = $conn->prepare("delete from loginLimit where ipAddress like :ip");
+            $ip = $this->getIp();
+            $statement->bindParam(":ip", $ip);
+
+            $statement->execute();
+        }
+
+        public function failedLoginAmount() {
+           //db conn
+           $conn = Db::getConnection();
+           //insert query
+           $statement = $conn->prepare( "select COUNT(*) from loginLimit where ipAddress like :ip and timeDiff > :diff");
+           $ip = $this->getIp();
+           $diff = $this->getDiff();
+           $statement->bindParam(":ip", $ip);
+           $statement->bindParam(":diff", $diff);
+
+           $statement->execute();
+           $count = $statement->fetch(PDO::FETCH_ASSOC);
+           $this->deleteFailedLogin();
+           return $count;
+        }
+    }
--- login.php
+++ login.php
@@ -1,29 +1,53 @@
 <?php 
 	include_once(__DIR__ . "/classes/User.php");
+	include_once(__DIR__ . "/classes/IpCheck.php");
 	$user = new User();
 
-	if(!empty($_POST)) {
-		$email = htmlspecialchars($_POST['email']);
-		$password = htmlspecialchars($_POST['password']);
-		if(!empty($email) && !empty($password)){
-			if($user->checkLogin($email, $password)){
-				$user->setEmail($email);
-				$idArray = $user->idFromSession($email);
-				$id = $idArray['id'];
-				$user->setId($id);
+	$failedLogin = new IpCheck();
+	$ip = $_SERVER['REMOTE_ADDR']; //getting the IP Address
+	$t=time(); //Storing time in variable
+	$diff = (time()-60); // Here 60 means 1 minutes 1*60 seconds
 
-				// later aanpassen -> if checkbox is ticked use cookie 
-				session_start();
-				$_SESSION["user"] = $email; 
-				$_SESSION["id"] = $id;
+	$failedLogin->setIp($ip);
+	$failedLogin->setT($t);
+	$failedLogin->setDiff($diff);
 
-				//redirect to index.php
-				header("Location: index.php");
-			}else{
-				$error = "Wachtwoord en email komen niet overeen";
+	$count = $failedLogin->failedLoginAmount();
+	if( $count["COUNT(*)"] > 3) {
+		$error = $count["COUNT(*)"] . " foutieve login pogingen, probeer binnen enkele minuten opnieuw";
+	}else {
+		if(!empty($_POST)) {
+			$email = htmlspecialchars($_POST['email']);
+			$password = htmlspecialchars($_POST['password']);
+			$count = $failedLogin->failedLoginAmount();
+			if(!empty($email) && !empty($password)){
+				if($user->checkLogin($email, $password)){
+					$user->setEmail($email);
+					$idArray = $user->idFromSession($email);
+					$id = $idArray['id'];
+					$user->setId($id);
+
+					// later aanpassen -> if checkbox is ticked use cookie 
+					session_start();
+					$_SESSION["user"] = $email; 
+					$_SESSION["id"] = $id;
+
+					//redirect to index.php
+					$failedLogin->deleteAllFailedLogin();
+					header("Location: index.php");
+				}else{
+					$num = 3 - $count["COUNT(*)"];
+					if($num > 0) {
+						$error = "Wachtwoord en email komen niet overeen, let op je hebt " . $num . " pogingen over";
+					}else {
+						$error = $count["COUNT(*)"] . " foutieve login pogingen, probeer binnen enkele minuten opnieuw";
+					}
+					
+					$failedLogin->failedLogin();	
+				}
+			}else {
+				$error = "email en wachtwoord zijn verplicht";
 			}
-		}else {
-			$error = "email en wachtwoord zijn verplicht";
 		}
 	}
 ?>
@@ -60,9 +84,9 @@
 					<label for="password">Passwoord</label>
                     <input type="password" id="password" name="password" placeholder="Passwoord">
                 </div>
-
+				
 				<div class="form__field">
-					<input type="submit" value="vervolledig profiel" class="btn btn--primary">	
+					<input type="submit" value="login" class="btn btn--primary">	
 				</div>
 			</form>
 		</div>
