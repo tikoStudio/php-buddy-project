2b5360545853e24a41107494ca3af7d3b2d7ca17
tikoStudio <timothy.koenig@mail.com>
2020-05-01 16:26:30 +0200

    small fixes in js files

--- js/filter.js
+++ js/filter.js
@@ -2,6 +2,5 @@ let removeFilterBtn = document.querySelector('.a__filterRemove')
 let filter = document.querySelector('.filter')
 
 removeFilterBtn.addEventListener('click', (e) => {
-    console.log("hey")
     filter.style.display = "none"
 })
