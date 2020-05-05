8a388c99586f98c74fa2efb0d1b149a03d230f67
Caro03 <caroverstreken@hotmail.com>
2020-05-05 15:46:50 +0200

    Changed some things from forum.php

--- forum.php
+++ forum.php
@@ -10,18 +10,10 @@ if (!isset($_SESSION['user'])) {
 $user = new User();
 $posts = $user->printPost();
 
-if(!empty($_POST)) {
+if(!empty($_POST['question'])) {
     try {
         $user->setPost($_POST['question']);
         $user->setId($_SESSION["id"]);
-    }
-    catch (\Throwable $th) {
-        $error = $th->getMessage();
-    }
-}
-
-if(!isset($error)) {
-    try {
         $user->savePost();
     }
     catch (\Throwable $th) {
@@ -29,7 +21,10 @@ if(!isset($error)) {
     }
 }
 
-
+if(!empty($_POST['pinit'])) {
+    $user->setId($_SESSION["id"]);
+    $user->savePin();
+}
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -41,31 +36,44 @@ if(!isset($error)) {
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <!--
-<?php include_once('nav.inc.php'); ?>
--->
+
+
+
 <div class="form__error forums"><?php if (isset($error)): ?><?php echo $error; ?><?php endif; ?></div>
 
 <form action="" method="POST">
-<div class="form_field forum">
+<div class="form_field forum-input">
     <textarea name="question" id="question" cols="30" rows="10" placeholder="Stel hier je vraag"></textarea>
 </div>
 
 <div class="form__field">
-    <input type="submit" value="Verzenden" class="btn btn--primary">
+    <input type="submit" value="Verzenden" class="btn btn--primary forum-btn">
 </div>
 </form>
 
 <div class="forum-posts">
 <?php foreach ($posts as $p): ?>
+<div class="forum-text">
 <h2 class="forum-naam"><?php echo htmlspecialchars($p['firstname']) . " " . htmlspecialchars($p['lastname']); ?></h2>
 <p class="forum-post"><?php echo htmlspecialchars($p['post']); ?></p>
-<div class="form_field forum">
-    <textarea name="answer" id="question" cols="30" rows="10" placeholder="Geef hier je antwoord"></textarea>
+<form action="" method="POST">
+<div class="form__field">
+    <input type="submit" value="Pin it" name="pinit" class="btn btn--primary forum-btn">
+</div>
+</form>
+</div>
+<!--
+<form action="" method="POST">
+<div class="forum-answer">
+<div class="form_field forum-input">
+    <input type="text" class="form-control" name="answer" id="lastname" placeholder="Geef hier je antwoord">
 </div>
 <div class="form__field">
-    <input type="submit" value="Verzenden" class="btn btn--primary">
+    <input type="submit" value="Verzenden" class="btn btn--primary forum-btn">
 </div>
+</div>
+</form>
+-->
 <?php endforeach; ?>
 </div>
 <?php include_once('footer.inc.php'); ?>
