e8b763345bf80696148789157534824f6fd29abe
Caro03 <caroverstreken@hotmail.com>
2020-05-05 15:47:00 +0200

    Add FAQ to nav

--- nav.inc.php
+++ nav.inc.php
@@ -4,6 +4,7 @@
                 <a href="index.php">Home</a>
                 <a href="friends.php">see all matches</a>
                 <a href="forum.php">forum</a>
+                <a href="faq.php">FAQ</a>
                 <a href="profile.php">profiel</a>
                 <a href="logout.php">logout</a>
         </nav>
