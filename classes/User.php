d292ceb57f846fcf5be226c17535fa4c83468289
Caro03 <caroverstreken@hotmail.com>
2020-05-05 19:45:58 +0200

    Add new class Post.php

--- classes/User.php
+++ classes/User.php
@@ -19,8 +19,6 @@
         protected $beverage;
         protected $pet;
         protected $active;
-        protected $post;
-        protected $pin;
         
 
         public function setFirstname($firstname)
@@ -441,87 +439,5 @@
             $this->active = $active;
 
             return $this;
-        }
-
-        public function savePost() {
-            $conn = Db::getConnection();
-            $statement = $conn->prepare("insert into posts (userId, post) values (:userId, :post)");
-            $userId = $this->getId();
-            $post =  $this->getPost();
-            $statement->bindValue(":userId", $userId);
-            $statement->bindValue(":post", $post);
-
-            $result = $statement->execute();
-            return $result;
-        }
-
-        /**
-         * Get the value of post
-         */ 
-        public function getPost()
-        {
-                return $this->post;
-        }
-
-        /**
-         * Set the value of post
-         *
-         * @return  self
-         */ 
-        public function setPost($post)
-        {
-            if (empty($post)) {
-                throw new Exception("Invulveld mag niet leeg zijn!");
-            }
-                $this->post = $post;
-
-                return $this;
-        }
-
-        public function printPost() {
-            $conn = Db::getConnection();
-            $statement = $conn->prepare("SELECT * from posts INNER JOIN users ON posts.userId = users.id ORDER BY date DESC");
-            $result = $statement->execute();
-            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        }
-
-        public function savePin() {
-            $conn = Db::getConnection();
-            $statement = $conn->prepare("update posts set pin = 1 where posts.id= :id");
-            $id = $this->getId();
-            $statement->bindParam(":id", $id);
-            //return result
-            $result = $statement->execute();
-            $result = $statement->fetch(PDO::FETCH_ASSOC);
-            return $result;
-        }
-
-        public function showPins() {
-            $conn = Db::getConnection();
-            $statement = $conn->prepare("select * from posts inner join users on posts.userId = users.id where pin= 1");
-            $result = $statement->execute();
-            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        }
-
-        /**
-         * Get the value of pin
-         */ 
-        public function getPin()
-        {
-                return $this->pin;
-        }
-
-        /**
-         * Set the value of pin
-         *
-         * @return  self
-         */ 
-        public function setPin($pin)
-        {
-                $this->pin = $pin;
-
-                return $this;
         }
     }
